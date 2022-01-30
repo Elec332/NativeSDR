27b5471513524faf6cfae3a64202b9f12e24a544
Elec332 <neen>
2022-01-31 00:36:55 +0100

    Proper split-up of find and use/install library functions

--- cmake/DefaultModuleConfig.cmake
+++ cmake/DefaultModuleConfig.cmake
@@ -1,11 +1,11 @@
 include(${CMAKE_CURRENT_LIST_DIR}/CMakeBase.cmake)
+set(MODULE_DIR modules)
 
 function(addModule)
     addProject(LIB SHARED)
 
-    useLibrary(NativeSDR::core)
-    useLibrary(NativeSDR::graphics)
+    useLibraries(NAMES NativeSDR::core NativeSDR::graphics PRIVATE)
 
-    installRuntime(DESTINATION modules)
+    installRuntime(DESTINATION ${MODULE_DIR})
     add_custom_target(${PROJECT_NAME}_run COMMAND NativeSDR::main -md ${CMAKE_CURRENT_BINARY_DIR})
 endfunction()
